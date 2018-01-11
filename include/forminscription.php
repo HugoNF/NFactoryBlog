442b9343589cdad824d90c362546e19c27088e55
HugoNF <32874545+HugoNF@users.noreply.github.com>
2018-01-11 15:08:19 +0100

    Update forminscription.php

--- include/forminscription.php
+++ include/forminscription.php
@@ -1,44 +1,18 @@
-<?php
-echo("<h1>Inscription</h1>");
-if(isset($_POST['formulaire'])){
-    $tabErreur = array();
-    $nom=$_POST["nom"];
-    $prenom=$_POST["prenom"];
-    $email=$_POST["email"];
-    $mdp=$_POST["password"];
-
-    if($_POST["nom"] == ""){
-        array_push($tabErreur,"Veuillez saisir votre nom");
-    }
-    if($_POST["prenom"] == ""){
-        array_push($tabErreur,"Veuillez saisir votre prénom");
-    }
-    if($_POST["email"] == ""){
-        array_push($tabErreur,"Veuillez saisir votre email");
-    }
-    if($_POST["password"] == ""){
-        array_push($tabErreur,"Veuillez saisir votre mot de passe");
-    }
-    if(count($tabErreur)!=0){
-        $message="<ul>";
-        for($i=0;$i<count($tabErreur);$i++){
-            $message .= "<li>".$tabErreur[$i]."</li>";
-        }
-        $message .= "</ul>";
-        echo($message);
-        include("./include/forminscription.php");
-    }else {
-        $connexion=mysqli_connect("localhost","root","","NFactoryBlog");
-        $mdp=sha1($_POST['password']);
-        $requete = "INSERT INTO t_users (ID_user, USERNAME, USERFNAME, USERMAIL, USERPASSWORD,USERDATEINS,T_ROLES_ID_ROLE) VALUES (NULL, '$nom', '$prenom','$email', '$mdp',NULL,5)";
-        mysqli_query($connexion,$requete);
-        mysqli_close($connexion);
-    }
-
-} else {
-    echo("Je viens d'ailleurs");
-    include("./include/forminscription.php");
-}
-
-?>
+<form method="post" action="#">
+    <div class="nom">
+        <label for="nom">Nom : </label><input type="text" name="nom">
+    </div>
+    <div class="prenom">
+        <label for="prenom">Prénom : </label><input type="text" name="prenom">
+    </div>
+    <div class="email">
+        <label for="email">Email : </label><input type="email" name="email">
+    </div>
+    <div class="password">
+        <label for="password">Mot de passe : </label><input type="password" name="password">
+    </div>
+    <div class="bouton">
+        <input type="submit" value="S'inscrire" name="formulaire">
+    </div>
+</form>
 
