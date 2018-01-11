73f676a0260aeec97425fe3e1b45f59295a86624
HugoNF <32874545+HugoNF@users.noreply.github.com>
2018-01-11 15:09:01 +0100

    Update inscription.inc.php

--- include/inscription.inc.php
+++ include/inscription.inc.php
@@ -1,39 +1,43 @@
-<h1>Inscription</h1>
 <?php
-if(isset($_POST["formulaire"])) {
+echo("<h1>Inscription</h1>");
+if(isset($_POST['formulaire'])){
     $tabErreur = array();
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $mail = $_POST['mail'];
-    $mdp = $_POST['mdp'];
-    if($_POST["nom"] == "")
-        array_push($tabErreur, "Veuillez saisir votre nom");
-    if($_POST["prenom"] == "")
-        array_push($tabErreur, "Veuillez saisir votre prénom");
-    if($_POST["mail"] == "")
-        array_push($tabErreur, "Veuillez saisir votre e-mail");
-    if($_POST["mdp"] == "")
-        array_push($tabErreur, "Veuillez saisir un mot de passe");
-    if(count($tabErreur) != 0) {
-        $message = "<ul>";
-        for($i = 0 ; $i < count($tabErreur) ; $i++) {
-            $message .= "<li>" . $tabErreur[$i] . "</li>";
+    $nom=$_POST["nom"];
+    $prenom=$_POST["prenom"];
+    $email=$_POST["email"];
+    $mdp=$_POST["password"];
+
+    if($_POST["nom"] == ""){
+        array_push($tabErreur,"Veuillez saisir votre nom");
+    }
+    if($_POST["prenom"] == ""){
+        array_push($tabErreur,"Veuillez saisir votre prénom");
+    }
+    if($_POST["email"] == ""){
+        array_push($tabErreur,"Veuillez saisir votre email");
+    }
+    if($_POST["password"] == ""){
+        array_push($tabErreur,"Veuillez saisir votre mot de passe");
+    }
+    if(count($tabErreur)!=0){
+        $message="<ul>";
+        for($i=0;$i<count($tabErreur);$i++){
+            $message .= "<li>".$tabErreur[$i]."</li>";
         }
         $message .= "</ul>";
         echo($message);
-        include("./include/formInscription.php");
-    }
-    else {
-        $connexion = mysqli_connect("localhost", "root", "", "nfactoryblog");
-        $requete = "INSERT INTO t_users (ID_USER, USERNAME, USERFNAME,
-                  USERMAIL, USERPASSWORD, USERDATEINS, T_ROLES_ID_ROLE)
-                  VALUES (NULL, '$nom', '$prenom', '$mail', '$mdp', NULL, 5);";
-        mysqli_query($connexion, $requete);
+        include("./include/forminscription.php");
+    }else {
+        $connexion=mysqli_connect("localhost","root","","NFactoryBlog");
+        $mdp=sha1($_POST['password']);
+        $requete = "INSERT INTO t_users (ID_user, USERNAME, USERFNAME, USERMAIL, USERPASSWORD,USERDATEINS,T_ROLES_ID_ROLE) VALUES (NULL, '$nom', '$prenom','$email', '$mdp',NULL,5)";
+        mysqli_query($connexion,$requete);
         mysqli_close($connexion);
-
     }
-}
-else {
+    
+} else {
     echo("Je viens d'ailleurs");
-    include("./include/formInscription.php");
+    include("./include/forminscription.php");
 }
+
+?>
