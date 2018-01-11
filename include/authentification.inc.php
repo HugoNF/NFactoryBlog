bf42eba2cd52990d70b9e97571caf9683586fdc8
HugoNF <32874545+HugoNF@users.noreply.github.com>
2018-01-11 15:07:14 +0100

    Update authentification.inc.php

--- include/authentification.inc.php
+++ include/authentification.inc.php
@@ -1,33 +1,46 @@
 <?php
-if(isset($_POST["formulaire"])) {
+
+echo("<h1>Login</h1>");
+if(isset($_POST['formulaire2'])){
     $tabErreur = array();
-    $mail = $_POST['mail'];
-    $mdp = $_POST['mdp'];
+    $email=$_POST["email"];
+    $mdp=$_POST["password"];
 
 
-    if($_POST["mail"] == "")
-        array_push($tabErreur, "Veuillez saisir votre e-mail");
-    if($_POST["mdp"] == "")
-        array_push($tabErreur, "Veuillez saisir un mot de passe");
-    if(count($tabErreur) != 0) {
-        $message = "<ul>";
-        for($i = 0 ; $i < count($tabErreur) ; $i++) {
-            $message .= "<li>" . $tabErreur[$i] . "</li>";
+    if($_POST["email"] == ""){
+        array_push($tabErreur,"Veuillez saisir votre email");
+    }
+    if($_POST["password"] == ""){
+        array_push($tabErreur,"Veuillez saisir votre mot de passe");
+    }
+    if(count($tabErreur)!=0){
+        $message="<ul>";
+        for($i=0;$i<count($tabErreur);$i++){
+            $message .= "<li>".$tabErreur[$i]."</li>";
         }
         $message .= "</ul>";
         echo($message);
-        include("./include/login.inc.php");
-    }
-    else {
-        $connexion = mysqli_connect("localhost", "root", "", "nfactoryblog");
-        $mdp=sha1($_POST['#mdp'])
-        $requete = "SELECT * FROM t_users WHERE USERMAIL='$mail' AND USERPASSWORD='$mdp'";
-        mysqli_query($connexion, $requete);
-        mysqli_close($connexion);
+        include("./include/login.php");
+    }else {
+        $connexion=mysqli_connect("localhost","root","","NFactoryBlog");
+        $mdp=sha1($_POST['password']);
+        $requete = "SELECT * FROM t_users WHERE USERMAIL='$email' AND USERPASSWORD='$mdp'";
 
+
+        mysqli_query($connexion,$requete);
+        if($result=mysqli_query($connexion,$requete)){
+            $row_cnt= mysqli_num_rows($result);
+            echo($row_cnt);
+            mysqli_free_result($result);
+        }
+        mysqli_close($connexion);
+        session_start();
+        $_SESSION['email']=$email;
     }
+
+} else {
+
+    include("./include/login.php");
 }
-else {
-    echo("Je viens d'ailleurs");
-    include("./include/login.inc.php");
-}
+
+?>
